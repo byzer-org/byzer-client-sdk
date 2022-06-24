35a713a63a802ba066eb24bd0b7f0fdc7906f62b
WilliamZhu <allwefantasy@gmail.com>
2022-06-24 12:12:47 +0800

    update

--- java-client/src/test/java/tech/mlsql/byzer_client_sdk/j_alng/test/JavaTest.java
+++ java-client/src/test/java/tech/mlsql/byzer_client_sdk/j_alng/test/JavaTest.java
@@ -3,6 +3,7 @@ package tech.mlsql.byzer_client_sdk.j_alng.test;
 import org.junit.jupiter.api.Test;
 import tech.mlsql.byzer_client_sdk.j_lang.generator.Byzer;
 import tech.mlsql.byzer_client_sdk.j_lang.generator.Expr;
+import tech.mlsql.byzer_client_sdk.scala_lang.generator.BaseNode;
 
 /**
  * 24/6/2022 WilliamZhu(allwefantasy@gmail.com)
@@ -11,11 +12,14 @@ public class JavaTest {
     @Test
     public void testJoin() {
         tech.mlsql.byzer_client_sdk.scala_lang.generator.Byzer builder = Byzer.builder();
-        builder.load().format("csv").path("/tmp/jack").options().add("header", "true").end().tag("table1").end();
-        builder.load().format("csv").path("/tmp/admond").options().add("header", "true").end().tag("table2").end();
+        BaseNode tableLoad1 = builder.load().format("csv").path("/tmp/jack").options().add("header", "true").end().tag("table1");
+        BaseNode  tableLoad2 = builder.load().format("csv").path("/tmp/admond").options().add("header", "true").end().tag("table2");
 
-        String table1 = builder.getByTag("table1").head().tableName();
-        String table2 = builder.getByTag("table2").head().tableName();
+        tableLoad1.end();
+        tableLoad2.end();
+
+        String table1 = tableLoad1.tableName();
+        String table2 = tableLoad2.tableName();
 
         System.out.println(table1);
         System.out.println(table2);
