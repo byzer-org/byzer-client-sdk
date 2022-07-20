a0a3dc2c937cf0ab9fd3631a54e5488cc8261613
admondguo <1006216254@qq.com>
2022-07-20 18:51:08 +0800

    revert

--- scala-client/src/main/java/tech/mlsql/byzer_client_sdk/scala_lang/generator/node/Columns.scala
+++ scala-client/src/main/java/tech/mlsql/byzer_client_sdk/scala_lang/generator/node/Columns.scala
@@ -72,8 +72,6 @@ class Columns(parent: Byzer) extends BaseNode {
   }
 
   override def end: Byzer = {
-    if (parent.blocks.size == 1 && _from.isEmpty)
-      throw new RuntimeException("First Node must call def from()!")
     _isReady = true
     parent
   }
--- scala-client/src/main/java/tech/mlsql/byzer_client_sdk/scala_lang/generator/node/ET.scala
+++ scala-client/src/main/java/tech/mlsql/byzer_client_sdk/scala_lang/generator/node/ET.scala
@@ -111,8 +111,6 @@ class ET(parent: Byzer) extends BaseNode {
   }
 
   override def end: Byzer = {
-    if (parent.blocks.size == 1 && _from.isEmpty)
-      throw new RuntimeException("First Node must call def from()!")
     _isReady = true
     parent
   }
